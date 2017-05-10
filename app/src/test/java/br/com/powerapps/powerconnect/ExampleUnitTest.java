d746370b11abe3e799a925c80a20f281dd7c2cbb
Marcus Eduardo <marcusedu7@gmail.com>
2017-05-10 14:23:01 -0300

    Configurado Java 8 Lambdas e RxJava/RxAndroid

--- app/src/test/java/br/com/powerapps/powerconnect/ExampleUnitTest.java
+++ app/src/test/java/br/com/powerapps/powerconnect/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package br.com.powerapps.powerconnect;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import io.reactivex.Observable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,12 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testeDeRxJavaNumerosParesLambdas() throws Exception {
+        Observable.just(1, 2, 4, 8, 6, 12, 34, 13)
+                .filter(num -> num % 2 == 0)
+                .subscribe(integer -> assertTrue(integer % 2 == 0));
     }
 }
